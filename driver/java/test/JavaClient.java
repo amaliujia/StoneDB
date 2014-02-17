9c2f30deb720ae9b6d8f93366c9df3877cfe410e
amaliujia <amaliujia@163.com>
2014-02-17 13:25:09 +0800

    java client test code

---
+++ driver/java/test/JavaClient.java
@@ -0,0 +1,17 @@
+import com.emeralddb.base.Emeralddb;
+
+public class JavaClient{
+	public static void main(String[] args)
+	{
+	   String addr = new String("127.0.0.1");
+	   int port = 48123;
+	   Emeralddb edb = new Emeralddb();
+	   edb.startStat();
+	   edb.init("./Configure");
+	   edb.insert("_id:212", "{_id:212,hello:1}");
+	   edb.query("{_id:212}");
+	   edb.delete("{_id:212}");
+	   edb.endStat(1);
+	   edb.disconnect();
+	}
+}
