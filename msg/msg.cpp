a6d3453fe6f7401e435c4de1e76e3b7fb486ef00
amaliujia <amaliujia@163.com>
2014-08-22 00:38:37 -0400

    ssssssss

--- msg/msg.cpp
+++ msg/msg.cpp
@@ -2,287 +2,286 @@
 #include "core.hpp"
 #include "pd.hpp"
 
-using namespace bson ;
-static int msgCheckBuffer ( char **ppBuffer, int *pBufferSize, int length )
+using namespace bson;
+static int msgCheckBuffer(char **ppBuffer, int *pBufferSize, int length)
 {
-   int rc = EDB_OK ;
-   if ( length > *pBufferSize )
+   int rc = EDB_OK;
+   if(length > *pBufferSize)
    {
-      char *pOldBuf = *ppBuffer ;
-      if ( length < 0 )
+      char *pOldBuf = *ppBuffer;
+      if(length < 0)
       {
-         PD_LOG ( PDERROR, "invalid length: %d", length ) ;
-         rc = EDB_INVALIDARG ;
-         goto error ;
+         PD_LOG(PDERROR, "invalid length: %d", length);
+         rc = EDB_INVALIDARG;
+         goto error;
       }
-      *ppBuffer = (char*)realloc ( *ppBuffer, sizeof(char)*length ) ;
-      if ( !*ppBuffer )
+      *ppBuffer = (char*)realloc(*ppBuffer, sizeof(char)*length);
+      if(!*ppBuffer)
       {
-         PD_LOG ( PDERROR, "Failed to allocate %d bytes buffer", length ) ;
-         rc = EDB_OOM ;
-         *ppBuffer = pOldBuf ;
-         goto error ;
+         PD_LOG(PDERROR, "Failed to allocate %d bytes buffer", length);
+         rc = EDB_OOM;
+         *ppBuffer = pOldBuf;
+         goto error;
       }
-      *pBufferSize = length ;
+      *pBufferSize = length;
    }
-done :
-   return rc ;
-error :
-   goto done ;
+done:
+   return rc;
+error:
+   goto done;
 }
 
-int msgBuildReply ( char **ppBuffer, int *pBufferSize, int returnCode,
-                    BSONObj *objReturn )
+int msgBuildReply(char **ppBuffer, int *pBufferSize, int returnCode,
+                    BSONObj *objReturn)
 {
-   int rc           = EDB_OK ;
-   int size         = sizeof ( MsgReply ) ;
-   MsgReply *pReply = NULL ;
+   int rc = EDB_OK;
+   int size = sizeof(MsgReply);
+   MsgReply *pReply = NULL;
 
-   if ( objReturn )
+   if(objReturn)
    {
-      size += objReturn->objsize() ;
+      size += objReturn->objsize();
    }
-   rc = msgCheckBuffer ( ppBuffer, pBufferSize, size ) ;
-   PD_RC_CHECK ( rc, PDERROR, "Failed to realloc buffer for %d bytes, rc = %d",
-                 size, rc ) ;
+   rc = msgCheckBuffer(ppBuffer, pBufferSize, size);
+   PD_RC_CHECK(rc, PDERROR, "Failed to realloc buffer for %d bytes, rc = %d",
+                 size, rc);
    // buffer is allocated, let's assign variables
-   pReply                    = (MsgReply *)(*ppBuffer) ;
+   pReply = (MsgReply *)(*ppBuffer);
    // build header
-   pReply->header.messageLen = size ;
-   pReply->header.opCode     = OP_REPLY ;
+   pReply->header.messageLen = size;
+   pReply->header.opCode = OP_REPLY;
    //build body
-   pReply->returnCode        = returnCode ;
-   pReply->numReturn         = (objReturn)?1:0 ;
+   pReply->returnCode = returnCode;
+   pReply->numReturn = (objReturn) ? 1 : 0;
    // bson object
-   if ( objReturn )
+   if(objReturn)
    {
-      memcpy ( &pReply->data[0], objReturn->objdata(), objReturn->objsize() ) ;
+      memcpy(&pReply->data[0], objReturn->objdata(), objReturn->objsize());
    }
-done :
-   return rc ;
-error :
-   goto done ;
+done:
+   return rc;
+error:
+   goto done;
 }
 
-
-int msgExtractReply ( char *pBuffer, int &returnCode, int &numReturn,
-                      const char **ppObjStart )
+int msgExtractReply(char *pBuffer, int &returnCode, int &numReturn,
+                      const char **ppObjStart)
 {
-   int rc              = EDB_OK ;
-   MsgReply *pReply    = (MsgReply*)pBuffer ;
+   int rc = EDB_OK;
+   MsgReply *pReply = (MsgReply*)pBuffer;
    // sanity check for header
-   if ( pReply->header.messageLen < (int)sizeof(MsgReply) )
+   if(pReply->header.messageLen < (int)sizeof(MsgReply))
    {
-      PD_LOG ( PDERROR, "Invalid length of reply message" ) ;
-      rc = EDB_INVALIDARG ;
-      goto error ;
+      PD_LOG(PDERROR, "Invalid length of reply message");
+      rc = EDB_INVALIDARG;
+      goto error;
    }
    // sanity check for opCode
-   if ( pReply->header.opCode != OP_REPLY )
+   if(pReply->header.opCode != OP_REPLY)
    {
-      PD_LOG ( PDERROR, "non-reply code is received: %d, expected %d",
-               pReply->header.opCode, OP_REPLY ) ;
-      rc = EDB_INVALIDARG ;
-      goto error ;
+      PD_LOG(PDERROR, "non-reply code is received: %d, expected %d",
+               pReply->header.opCode, OP_REPLY);
+      rc = EDB_INVALIDARG;
+      goto error;
    }
    // extract
-   returnCode = pReply->returnCode ;
-   numReturn  = pReply->numReturn ;
+   returnCode = pReply->returnCode;
+   numReturn  = pReply->numReturn;
    // object
-   if ( 0 == numReturn )
+   if(0 == numReturn)
    {
-      *ppObjStart = NULL ;
+      *ppObjStart = NULL;
    }
    else
    {
-      *ppObjStart = &pReply->data[0] ;
+      *ppObjStart = &pReply->data[0];
    }
-done :
-   return rc ;
-error :
-   goto done ;
+done:
+   return rc;
+error:
+   goto done;
 }
 
-int msgBuildInsert ( char **ppBuffer, int *pBufferSize, BSONObj &obj )
+int msgBuildInsert(char **ppBuffer, int *pBufferSize, BSONObj &obj)
 {
-   int rc             = EDB_OK ;
-   int size           = sizeof(MsgInsert) + obj.objsize() ;
-   MsgInsert *pInsert = NULL ;
-   rc = msgCheckBuffer ( ppBuffer, pBufferSize, size ) ;
-   if ( rc )
+   int rc = EDB_OK;
+   int size = sizeof(MsgInsert) + obj.objsize();
+   MsgInsert *pInsert = NULL;
+   rc = msgCheckBuffer(ppBuffer, pBufferSize, size);
+   if(rc)
    {
-      PD_LOG ( PDERROR, "Failed to realloc buffer for %d bytes, rc = %d",
-               size, rc ) ;
-      goto error ;
+      PD_LOG(PDERROR, "Failed to realloc buffer for %d bytes, rc = %d",
+               size, rc);
+      goto error;
    }
    // buffer is allocated, let's assign variables
-   pInsert                    = (MsgInsert*)(*ppBuffer) ;
+   pInsert = (MsgInsert*)(*ppBuffer);
    // build header
-   pInsert->header.messageLen = size ;
-   pInsert->header.opCode     = OP_INSERT ;
+   pInsert->header.messageLen = size;
+   pInsert->header.opCode = OP_INSERT;
    // build body
-   pInsert->numInsert         = 1 ;
+   pInsert->numInsert = 1;
    // bson object
-   memcpy ( &pInsert->data[0], obj.objdata(), obj.objsize() ) ;
-done :
-   return rc ;
-error :
-   goto done ;
+   memcpy(&pInsert->data[0], obj.objdata(), obj.objsize());
+done:
+   return rc;
+error:
+   goto done;
 }
 
-int msgBuildInsert ( char **ppBuffer, int *pBufferSize, vector<BSONObj*> &obj )
+int msgBuildInsert(char **ppBuffer, int *pBufferSize, vector<BSONObj*> &obj)
 {
-   int rc             = EDB_OK ;
-   int size           = sizeof(MsgInsert) ;
-   MsgInsert *pInsert = NULL ;
-   vector<BSONObj*>::iterator it ;
-   char *p            = NULL ;
-   for ( it = obj.begin(); it != obj.end(); ++it )
+   int rc = EDB_OK;
+   int size = sizeof(MsgInsert);
+   MsgInsert *pInsert = NULL;
+   vector<BSONObj*>::iterator it;
+   char *p = NULL;
+   for(it = obj.begin(); it != obj.end(); ++it)
    {
-      size += (*it)->objsize() ;
+      size += (*it)->objsize();
    }
-   rc = msgCheckBuffer ( ppBuffer, pBufferSize, size ) ;
-   if ( rc )
+   rc = msgCheckBuffer(ppBuffer, pBufferSize, size);
+   if(rc)
    {
-      PD_LOG ( PDERROR, "Failed to realloc buffer for %d bytes, rc = %d",
-               size, rc ) ;
-      goto error ;
+      PD_LOG(PDERROR, "Failed to realloc buffer for %d bytes, rc = %d",
+               size, rc);
+      goto error;
    }
    // buffer is allocated, let's assign variables
-   pInsert                    = (MsgInsert*)(*ppBuffer) ;
+   pInsert = (MsgInsert*)(*ppBuffer);
    // build header
-   pInsert->header.messageLen = size ;
-   pInsert->header.opCode     = OP_INSERT ;
+   pInsert->header.messageLen = size;
+   pInsert->header.opCode = OP_INSERT;
    // build body
-   pInsert->numInsert         = obj.size() ;
+   pInsert->numInsert = obj.size();
    // bson object
-   p = &pInsert->data[0] ;
-   for ( it = obj.begin(); it != obj.end(); ++it )
+   p = &pInsert->data[0];
+   for(it = obj.begin(); it != obj.end(); ++it)
    {
-      memcpy ( p, (*it)->objdata(), (*it)->objsize() ) ;
-      p += (*it)->objsize() ;
+      memcpy(p, (*it)->objdata(), (*it)->objsize());
+      p += (*it)->objsize();
    }
-done :
-   return rc ;
-error :
-   goto done ;
+done:
+   return rc;
+error:
+   goto done;
 }
 
-int msgExtractInsert ( char *pBuffer, int &numInsert, const char **ppObjStart )
+int msgExtractInsert(char *pBuffer, int &numInsert, const char **ppObjStart)
 {
-   int rc              = EDB_OK ;
-   MsgInsert *pInsert  = (MsgInsert*)pBuffer ;
+   int rc = EDB_OK;
+   MsgInsert *pInsert  = (MsgInsert*)pBuffer;
    // sanity check for header
-   if ( pInsert->header.messageLen < (int)sizeof(MsgInsert) )
+   if(pInsert->header.messageLen < (int)sizeof(MsgInsert))
    {
-      PD_LOG ( PDERROR, "Invalid length of insert message" ) ;
-      rc = EDB_INVALIDARG ;
-      goto error ;
+      PD_LOG(PDERROR, "Invalid length of insert message");
+      rc = EDB_INVALIDARG;
+      goto error;
    }
    // sanity check for opCode
-   if ( pInsert->header.opCode != OP_INSERT )
+   if(pInsert->header.opCode != OP_INSERT)
    {
-      PD_LOG ( PDERROR, "non-insert code is received: %d, expected %d",
-               pInsert->header.opCode, OP_INSERT ) ;
-      rc = EDB_INVALIDARG ;
-      goto error ;
+      PD_LOG(PDERROR, "non-insert code is received: %d, expected %d",
+               pInsert->header.opCode, OP_INSERT);
+      rc = EDB_INVALIDARG;
+      goto error;
    }
    // extract
-   numInsert  = pInsert->numInsert ;
+   numInsert  = pInsert->numInsert;
    // object
-   if ( 0 == numInsert )
+   if(0 == numInsert)
    {
-      *ppObjStart = NULL ;
+      *ppObjStart = NULL;
    }
    else
    {
-      *ppObjStart = &pInsert->data[0] ;
+      *ppObjStart = &pInsert->data[0];
    }
-done :
-   return rc ;
-error :
-   goto done ;
+done:
+   return rc;
+error:
+   goto done;
 }
 
-int msgBuildDelete ( char **ppBuffer, int *pBufferSize, BSONObj &key )
+int msgBuildDelete(char **ppBuffer, int *pBufferSize, BSONObj &key)
 {
-   int rc             = EDB_OK ;
-   int size           = sizeof(MsgDelete) + key.objsize() ;
-   MsgDelete *pDelete = NULL ;
-   rc = msgCheckBuffer ( ppBuffer, pBufferSize, size ) ;
-   if ( rc )
+   int rc = EDB_OK;
+   int size = sizeof(MsgDelete) + key.objsize();
+   MsgDelete *pDelete = NULL;
+   rc = msgCheckBuffer(ppBuffer, pBufferSize, size);
+   if(rc)
    {
-      PD_LOG ( PDERROR, "Failed to realloc buffer for %d bytes, rc = %d",
-               size, rc ) ;
-      goto error ;
+      PD_LOG(PDERROR, "Failed to realloc buffer for %d bytes, rc = %d",
+               size, rc);
+      goto error;
    }
    // buffer is allocated, let's assign variables
-   pDelete                    = (MsgDelete*)(*ppBuffer) ;
+   pDelete = (MsgDelete*)(*ppBuffer);
    // build header
-   pDelete->header.messageLen = size ;
-   pDelete->header.opCode     = OP_DELETE ;
+   pDelete->header.messageLen = size;
+   pDelete->header.opCode = OP_DELETE;
    // bson object
-   memcpy ( &pDelete->key[0], key.objdata(), key.objsize() ) ;
-done :
-   return rc ;
-error :
-   goto done ;
+   memcpy(&pDelete->key[0], key.objdata(), key.objsize());
+done:
+   return rc;
+error:
+   goto done;
 }
 
-int msgExtractDelete ( char *pBuffer, BSONObj &key )
+int msgExtractDelete(char *pBuffer, BSONObj &key)
 {
-   int rc              = EDB_OK ;
-   MsgDelete *pDelete  = (MsgDelete*)pBuffer ;
+   int rc = EDB_OK;
+   MsgDelete *pDelete = (MsgDelete*)pBuffer;
    // sanity check for header
-   if ( pDelete->header.messageLen < (int)sizeof(MsgDelete) )
+   if(pDelete->header.messageLen < (int)sizeof(MsgDelete))
    {
-      PD_LOG ( PDERROR, "Invalid length of delete message" ) ;
-      rc = EDB_INVALIDARG ;
-      goto error ;
+      PD_LOG(PDERROR, "Invalid length of delete message");
+      rc = EDB_INVALIDARG;
+      goto error;
    }
    // sanity check for opCode
-   if ( pDelete->header.opCode != OP_DELETE )
+   if(pDelete->header.opCode != OP_DELETE)
    {
-      PD_LOG ( PDERROR, "non-delete code is received: %d, expected %d",
-               pDelete->header.opCode, OP_DELETE ) ;
-      rc = EDB_INVALIDARG ;
-      goto error ;
+      PD_LOG(PDERROR, "non-delete code is received: %d, expected %d",
+               pDelete->header.opCode, OP_DELETE);
+      rc = EDB_INVALIDARG;
+      goto error;
    }
    // extract
    // object
-   key = BSONObj ( &pDelete->key[0] ) ;
-done :
-   return rc ;
-error :
-   goto done ;
+   key = BSONObj(&pDelete->key[0]);
+done:
+   return rc;
+error:
+   goto done;
 }
 
-int msgBuildQuery ( char **ppBuffer, int *pBufferSize, BSONObj &key )
+int msgBuildQuery(char **ppBuffer, int *pBufferSize, BSONObj &key)
 {
-   int rc             = EDB_OK ;
-   int size           = sizeof(MsgQuery) + key.objsize() ;
-   MsgQuery *pQuery   = NULL ;
-   rc = msgCheckBuffer ( ppBuffer, pBufferSize, size ) ;
-   if ( rc )
+   int rc = EDB_OK;
+   int size = sizeof(MsgQuery) + key.objsize();
+   MsgQuery *pQuery = NULL;
+   rc = msgCheckBuffer(ppBuffer, pBufferSize, size);
+   if(rc)
    {
-      PD_LOG ( PDERROR, "Failed to realloc buffer for %d bytes, rc = %d",
-               size, rc ) ;
-      goto error ;
+      PD_LOG(PDERROR, "Failed to realloc buffer for %d bytes, rc = %d",
+               size, rc);
+      goto error;
    }
    // buffer is allocated, let's assign variables
-   pQuery                    = (MsgQuery*)(*ppBuffer) ;
+   pQuery = (MsgQuery*)(*ppBuffer);
    // build header
-   pQuery->header.messageLen = size ;
-   pQuery->header.opCode     = OP_QUERY ;
+   pQuery->header.messageLen = size;
+   pQuery->header.opCode = OP_QUERY;
    // bson object
-   memcpy ( &pQuery->key[0], key.objdata(), key.objsize() ) ;
-done :
-   return rc ;
-error :
-   goto done ;
+   memcpy(&pQuery->key[0], key.objdata(), key.objsize());
+done:
+   return rc;
+error:
+   goto done;
 }
 
-int msgExtractQuery ( char *pBuffer, BSONObj &key )
+int msgExtractQuery ( char *pBuffer, BSONObj &key)
 {
    int rc              = EDB_OK ;
    MsgQuery *pQuery    = (MsgQuery*)pBuffer ;
@@ -310,103 +309,103 @@ error :
    goto done ;
 }
 
-int msgBuildCommand ( char **ppBuffer, int *pBufferSize, BSONObj &obj )
+int msgBuildCommand(char **ppBuffer, int *pBufferSize, BSONObj &obj)
 {
-   int rc               = EDB_OK ;
-   int size             = sizeof(MsgCommand) + obj.objsize() ;
-   MsgCommand *pCommand = NULL ;
-   rc = msgCheckBuffer ( ppBuffer, pBufferSize, size ) ;
-   if ( rc )
+   int rc = EDB_OK;
+   int size = sizeof(MsgCommand) + obj.objsize();
+   MsgCommand *pCommand = NULL;
+   rc = msgCheckBuffer(ppBuffer, pBufferSize, size);
+   if(rc)
    {
-      PD_LOG ( PDERROR, "Failed to realloc buffer for %d bytes, rc = %d",
-               size, rc ) ;
-      goto error ;
+      PD_LOG(PDERROR, "Failed to realloc buffer for %d bytes, rc = %d",
+               size, rc);
+      goto error;
    }
    // buffer is allocated, let's assign variables
-   pCommand                    = (MsgCommand*)(*ppBuffer) ;
+   pCommand = (MsgCommand*)(*ppBuffer);
    // build header
-   pCommand->header.messageLen = size ;
-   pCommand->header.opCode     = OP_COMMAND ;
+   pCommand->header.messageLen = size;
+   pCommand->header.opCode = OP_COMMAND;
    // build body
-   pCommand->numArgs           = 1 ;
+   pCommand->numArgs = 1;
    // bson object
-   memcpy ( &pCommand->data[0], obj.objdata(), obj.objsize() ) ;
-done :
-   return rc ;
-error :
-   goto done ;
+   memcpy(&pCommand->data[0], obj.objdata(), obj.objsize());
+done:
+   return rc;
+error:
+   goto done;
 }
 
-int msgBuildCommand ( char **ppBuffer, int *pBufferSize, vector<BSONObj*>&obj )
+int msgBuildCommand(char **ppBuffer, int *pBufferSize, vector<BSONObj*>&obj)
 {
-   int rc               = EDB_OK ;
-   int size             = sizeof(MsgCommand) ;
-   MsgCommand *pCommand = NULL ;
-   vector<BSONObj*>::iterator it ;
-   char *p            = NULL ;
-   for ( it = obj.begin(); it != obj.end(); ++it )
+   int rc = EDB_OK;
+   int size = sizeof(MsgCommand);
+   MsgCommand *pCommand = NULL;
+   vector<BSONObj*>::iterator it;
+   char *p = NULL;
+   for(it = obj.begin(); it != obj.end(); ++it)
    {
-      size += (*it)->objsize() ;
+      size += (*it)->objsize();
    }
-   rc = msgCheckBuffer ( ppBuffer, pBufferSize, size ) ;
-   if ( rc )
+   rc = msgCheckBuffer(ppBuffer, pBufferSize, size);
+   if(rc)
    {
-      PD_LOG ( PDERROR, "Failed to realloc buffer for %d bytes, rc = %d",
-               size, rc ) ;
-      goto error ;
+      PD_LOG(PDERROR, "Failed to realloc buffer for %d bytes, rc = %d",
+               size, rc);
+      goto error;
    }
    // buffer is allocated, let's assign variables
-   pCommand                    = (MsgCommand*)(*ppBuffer) ;
+   pCommand = (MsgCommand*)(*ppBuffer);
    // build header
-   pCommand->header.messageLen = size ;
-   pCommand->header.opCode     = OP_COMMAND ;
+   pCommand->header.messageLen = size;
+   pCommand->header.opCode = OP_COMMAND;
    // build body
-   pCommand->numArgs           = obj.size() ;
+   pCommand->numArgs = obj.size();
    // bson object
-   p = &pCommand->data[0] ;
-   for ( it = obj.begin(); it != obj.end(); ++it )
+   p = &pCommand->data[0];
+   for ( it = obj.begin(); it != obj.end(); ++it)
    {
-      memcpy ( p, (*it)->objdata(), (*it)->objsize() ) ;
-      p += (*it)->objsize() ;
+      memcpy ( p, (*it)->objdata(), (*it)->objsize());
+      p += (*it)->objsize();
    }
-done :
-   return rc ;
-error :
-   goto done ;
+done:
+   return rc;
+error:
+   goto done;
 }
 
-int msgExtractCommand ( char *pBuffer, int &numArgs, const char **ppObjStart )
+int msgExtractCommand(char *pBuffer, int &numArgs, const char **ppObjStart)
 {
-   int rc                = EDB_OK ;
-   MsgCommand *pCommand  = (MsgCommand*)pBuffer ;
+   int rc = EDB_OK;
+   MsgCommand *pCommand  = (MsgCommand*)pBuffer;
    // sanity check for header
-   if ( pCommand->header.messageLen < (int)sizeof(MsgCommand) )
+   if(pCommand->header.messageLen < (int)sizeof(MsgCommand))
    {
-      PD_LOG ( PDERROR, "Invalid length of command message" ) ;
-      rc = EDB_INVALIDARG ;
-      goto error ;
+      PD_LOG(PDERROR, "Invalid length of command message");
+      rc = EDB_INVALIDARG;
+      goto error;
    }
    // sanity check for opCode
-   if ( pCommand->header.opCode != OP_COMMAND )
+   if(pCommand->header.opCode != OP_COMMAND)
    {
-      PD_LOG ( PDERROR, "non-command code is received: %d, expected %d",
-               pCommand->header.opCode, OP_COMMAND ) ;
-      rc = EDB_INVALIDARG ;
-      goto error ;
+      PD_LOG(PDERROR, "non-command code is received: %d, expected %d",
+               pCommand->header.opCode, OP_COMMAND);
+      rc = EDB_INVALIDARG;
+      goto error;
    }
    // extract
-   numArgs  = pCommand->numArgs ;
+   numArgs  = pCommand->numArgs;
    // object
-   if ( 0 == numArgs )
+   if(0 == numArgs)
    {
-      *ppObjStart = NULL ;
+      *ppObjStart = NULL;
    }
    else
    {
-      *ppObjStart = &pCommand->data[0] ;
+      *ppObjStart = &pCommand->data[0];
    }
-done :
-   return rc ;
-error :
-   goto done ;
+done:
+   return rc;
+error:
+   goto done;
 }
--- oss/ossMmapFile.cpp
+++ oss/ossMmapFile.cpp
@@ -2,95 +2,95 @@
 #include "pd.hpp"
 
 using namespace std ;
-int _ossMmapFile::open ( const char * pFilename,
-                         unsigned int options )
+int _ossMmapFile::open(const char * pFilename,
+                         unsigned int options)
 {
-   int rc = EDB_OK ;
-   _mutex.get() ;
-   rc = _fileOp.Open ( pFilename, options ) ;
-   if ( EDB_OK == rc )
-      _opened = true ;
+   int rc = EDB_OK;
+   _mutex.get();
+   rc = _fileOp.Open(pFilename, options);
+   if(EDB_OK == rc)
+      _opened = true;
    else
    {
-      PD_LOG ( PDERROR, "Failed to open file, rc = %d",
-               rc ) ;
-      goto error ;
+      PD_LOG(PDERROR, "Failed to open file, rc = %d",
+               rc);
+      goto error;
    }
-   strncpy ( _fileName, pFilename, sizeof(_fileName) ) ;
-done :
-   _mutex.release () ;
-   return rc ;
-error :
-   goto done ;
+   strncpy(_fileName, pFilename, sizeof(_fileName));
+done:
+   _mutex.release();
+   return rc;
+error:
+   goto done;
 }
 
-void _ossMmapFile::close ()
+void _ossMmapFile::close()
 {
-   _mutex.get() ;
-   for ( vector<ossMmapSegment>::iterator i = _segments.begin() ;
-         i != _segments.end(); i++ )
+   _mutex.get();
+   for(vector<ossMmapSegment>::iterator i = _segments.begin();
+         i != _segments.end(); i++)
    {
-      munmap ((void*)(*i)._ptr, (*i)._length ) ;
+      munmap((void*)(*i)._ptr, (*i)._length);
    }
-   _segments.clear () ;
-   if ( _opened )
+   _segments.clear();
+   if(_opened)
    {
-      _fileOp.Close () ;
-      _opened = false ;
+      _fileOp.Close();
+      _opened = false;
    }
-   _mutex.release () ;
+   _mutex.release();
 }
 
-int _ossMmapFile::map ( unsigned long long offset,
+int _ossMmapFile::map(unsigned long long offset,
                         unsigned int length,
-                        void **pAddress )
+                        void **pAddress)
 {
-   _mutex.get() ;
-   int rc = EDB_OK ;
-   ossMmapSegment seg ( 0, 0, 0 ) ;
-   unsigned long long fileSize = 0 ;
-   void *segment = NULL ;
-   if ( 0 == length )
-      goto done ;
-   rc = _fileOp.getSize ((off_t*)&fileSize ) ;
-   if ( rc )
+   _mutex.get();
+   int rc = EDB_OK;
+   ossMmapSegment seg( 0, 0, 0);
+   unsigned long long fileSize = 0;
+   void *segment = NULL;
+   if(0 == length)
+      goto done;
+   rc = _fileOp.getSize((off_t*)&fileSize);
+   if(rc)
    {
-      PD_LOG ( PDERROR,
-               "Failed to get file size, rc = %d", rc ) ;
-      goto error ;
+      PD_LOG(PDERROR,
+             "Failed to get file size, rc = %d", rc);
+      goto error;
    }
-   if ( offset + length > fileSize )
+   if(offset + length > fileSize)
    {
-      PD_LOG ( PDERROR,
-               "Offset is greater than file size" ) ;
-      rc = EDB_INVALIDARG ;
-      goto error ;
+      PD_LOG(PDERROR,
+             "Offset is greater than file size");
+      rc = EDB_INVALIDARG;
+      goto error;
    }
    // map region into memory
-   segment = mmap ( NULL, length, PROT_READ | PROT_WRITE,
-                    MAP_SHARED, _fileOp.getHandle () , offset ) ;
-   if ( MAP_FAILED == segment )
+   segment = mmap(NULL, length, PROT_READ | PROT_WRITE,
+                  MAP_SHARED, _fileOp.getHandle () , offset);
+   if(MAP_FAILED == segment)
    {
-      PD_LOG ( PDERROR,
-               "Failed to map offset %ld length %d, errno = %d",
-               offset, length, errno ) ;
-      if ( ENOMEM == errno )
-         rc = EDB_OOM ;
-      else if ( EACCES == errno )
-         rc = EDB_PERM ;
+      PD_LOG(PDERROR,
+             "Failed to map offset %ld length %d, errno = %d",
+               offset, length, errno);
+      if(ENOMEM == errno)
+         rc = EDB_OOM;
+      else if(EACCES == errno)
+         rc = EDB_PERM;
       else
-         rc = EDB_SYS ;
-      goto error ;
+         rc = EDB_SYS;
+      goto error;
    }
-   seg._ptr = segment ;
-   seg._length = length ;
-   seg._offset = offset ;
-   _segments.push_back ( seg ) ;
-   if ( pAddress )
-      *pAddress = segment ;
-done :
-   _mutex.release () ;
-   return rc ;
-error :
-   goto done ;
+   seg._ptr = segment;
+   seg._length = length;
+   seg._offset = offset;
+   _segments.push_back(seg);
+   if(pAddress)
+      *pAddress = segment;
+done:
+   _mutex.release();
+   return rc;
+error:
+   goto done;
 }
