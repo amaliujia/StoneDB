820e138ec59635d1e65fb7c4903ef6186a4c7e55
amaliujia <amaliujia@163.com>
2015-04-12 00:02:51 -0400

    add sequential insert without time interval

--- driver/java/test/JavaClient.java
+++ driver/java/test/JavaClient.java
@@ -1,4 +1,5 @@
 import com.emeralddb.base.Emeralddb;
+import com.emeralddb.util.RandomGUID;
 
 public class JavaClient{
 	public static void main(String[] args)
@@ -8,9 +9,26 @@ public class JavaClient{
 	   Emeralddb edb = new Emeralddb();
 	   edb.startStat();
 	   edb.init("Configure");
-	   edb.insert("_id:212", "{_id:212,hello:1}");
-	   edb.query("{_id:212}");
-	   edb.delete("{_id:212}");
+//	   edb.insert("_id:212", "{_id:212,hello:1}");
+//	   edb.query("{_id:212}");
+//	   edb.delete("{_id:212}");
+
+		int recordNum = 10;
+		String[] uuidArray = null;
+		uuidArray = new String[ recordNum ];
+		for( int i = 0; i < recordNum; i++ )
+		{
+			String uuid = java.util.UUID.randomUUID().toString().replaceAll( "-","" )
+					+ RandomGUID.newGuid();
+			uuidArray[i] = uuid;
+		}
+
+		for(int i = 0; i < recordNum; i++){
+			edb.insert(uuidArray[i], String.format( "{_id:'%s'}", uuidArray[i]));
+			edb.query( String.format( "{_id:'%s'}", uuidArray[i]));
+			edb.delete(String.format( "{_id:'%s'}", uuidArray[i]));
+		}
+
 	   edb.endStat(1);
 	   edb.disconnect();
 	}
